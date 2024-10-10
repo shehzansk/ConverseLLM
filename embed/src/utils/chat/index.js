fae3a737df2249e7de02b6159b3a1cab384a1acf
kohadved <128896945+kohadved@users.noreply.github.com>
2024-10-11 00:44:42 +0530

    Added embed chat feature

---
+++ embed/src/utils/chat/index.js
@@ -0,0 +1,110 @@
+// For handling of synchronous chats that are not utilizing streaming or chat requests.
+export default function handleChat(
+  chatResult,
+  setLoadingResponse,
+  setChatHistory,
+  remHistory,
+  _chatHistory
+) {
+  const {
+    uuid,
+    textResponse,
+    type,
+    sources = [],
+    error,
+    close,
+    errorMsg = null,
+  } = chatResult;
+
+  if (type === "abort") {
+    setLoadingResponse(false);
+    setChatHistory([
+      ...remHistory,
+      {
+        uuid,
+        content: textResponse,
+        role: "assistant",
+        sources,
+        closed: true,
+        error,
+        errorMsg,
+        animate: false,
+        pending: false,
+      },
+    ]);
+    _chatHistory.push({
+      uuid,
+      content: textResponse,
+      role: "assistant",
+      sources,
+      closed: true,
+      error,
+      errorMsg,
+      animate: false,
+      pending: false,
+    });
+  } else if (type === "textResponse") {
+    setLoadingResponse(false);
+    setChatHistory([
+      ...remHistory,
+      {
+        uuid,
+        content: textResponse,
+        role: "assistant",
+        sources,
+        closed: close,
+        error,
+        errorMsg,
+        animate: !close,
+        pending: false,
+      },
+    ]);
+    _chatHistory.push({
+      uuid,
+      content: textResponse,
+      role: "assistant",
+      sources,
+      closed: close,
+      error,
+      errorMsg,
+      animate: !close,
+      pending: false,
+    });
+  } else if (type === "textResponseChunk") {
+    const chatIdx = _chatHistory.findIndex((chat) => chat.uuid === uuid);
+    if (chatIdx !== -1) {
+      const existingHistory = { ..._chatHistory[chatIdx] };
+      const updatedHistory = {
+        ...existingHistory,
+        content: existingHistory.content + textResponse,
+        sources,
+        error,
+        errorMsg,
+        closed: close,
+        animate: !close,
+        pending: false,
+      };
+      _chatHistory[chatIdx] = updatedHistory;
+    } else {
+      _chatHistory.push({
+        uuid,
+        sources,
+        error,
+        errorMsg,
+        content: textResponse,
+        role: "assistant",
+        closed: close,
+        animate: !close,
+        pending: false,
+      });
+    }
+    setChatHistory([..._chatHistory]);
+  }
+}
+
+export function chatPrompt(workspace) {
+  return (
+    workspace?.openAiPrompt ??
+    "Given the following conversation, relevant context, and a follow up question, reply with an answer to the current question the user is asking. Return only your response to the question given the above information following the users instructions as needed."
+  );
+}
